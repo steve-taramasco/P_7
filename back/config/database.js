e58129317b766fa81b6e7dc94c3a53275b720fc0
steve <steve.taramasco@gmail.com>
2021-02-23 17:21:38 +0100

    .env

--- back/config/database.js
+++ back/config/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-module.exports = new Sequelize('groupomania', 'root', 'root', {
-    host: 'localhost',
+module.exports = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false,
     dateStrings: 'date',
